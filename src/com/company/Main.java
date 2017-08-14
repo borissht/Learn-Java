5d52aabc3e767decf278e0215f88cf196ca43e79
borissht <31014011+borissht@users.noreply.github.com>
2017-08-14 21:45:50 +0300

    test1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here jhgjhg
+	// write your code here jhgjhg test123
         
     }
 }
